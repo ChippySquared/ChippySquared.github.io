b3e30d3205c25be5ab5c50318c83ab2c7e2980ff
ChippySquared <160559375+ChippySquared@users.noreply.github.com>
2024-09-03 01:45:20 +0100

    Platformer v1.0

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `PC: This is where the IDEAS and BREAKTHROUGHS happen.
+    Currently running on Windows, Pop!_OS, and s0da.`,
+    "bootcamp-certificate": `Bootcamp Certificate: I saw there's plenty of 
+    space on the wall to hang things so made sure this was one of them 😁`,
+    "sofa-table": `Feel free to take a seat and 
+    a cup of tea after you've had a look around 🍵`,
+    tv: `Good ole tv. I don't watch it - it's a nice decoration though 😂`,
+    bed: `Devs actually sleep believe it or not 💯`,
+    resume: `CV: for a CV copy, please send an email to:
+    CodingWithBryan@proton.me`,
+    projects: `Profile: check out my <a href="https://github.com/ChippySquared"
+    target="_blank">GitHub</a> and <a href="
+    https://www.linkedin.com/in/codingbryan/" target="_blank">LinkedIn</a>!`,
+    library: `This project is written in HTML, CSS and JavaScript with the
+    help of the Kaboom.js library. A special thanks and credits to 
+    @JSLegendDev for the project inspiration 🎮`,
+    exit: `Entrance/Exit here🚪`
+};
